2ba5723f5930bb3530324467c953a7d477649bbf
Jorge Diaz <jorge.diaz@wolox.co>
2020-04-14 15:01:39 -0500

    greeting resource, create  BookController and greeting view

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,5 +1,18 @@
 package wolox.training.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class BookController {
+
+  @GetMapping("/greeting")
+  public String greeting(@RequestParam(name = "name", defaultValue = "world") String name,
+                         Model model) {
+    model.addAttribute("name", name);
+    return "greeting";
+  }
 
 }
