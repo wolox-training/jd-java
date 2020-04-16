5a36fc5fa39110ae7f3da24a42cea1ce5657535e
Jorge Diaz <jorge.diaz@wolox.co>
2020-04-16 11:35:50 -0500

    Raise exception if have duplicates

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -11,59 +11,55 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import wolox.training.exceptions.BookAlreadyOwnedException;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class User {
 
-  @Id
-  @GeneratedValue
-  private long id;
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private long id;
 
-  @Column(nullable = false, unique = true)
-  private String username;
+    @Column(nullable = false, unique = true)
+    private String username;
 
-  @Column(nullable = false)
-  private LocalDate birthDate;
+    @Column(nullable = false)
+    private LocalDate birthDate;
 
-  @ManyToMany
-  @JoinTable(name = "users_books",
-    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-    inverseJoinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"))
-  private List<Book> books;
+    @ManyToMany
+    @JoinTable(name = "users_books",
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"))
+    private List<Book> books;
 
-  public User() {
-  }
+    public List<Book> getBooks() {
+        return (List<Book>) Collections.unmodifiableList(books);
+    }
 
-  public long getId() {
-    return id;
-  }
+    public void setBooks(List<Book> books) throws BookAlreadyOwnedException {
+        if (haveDuplicateBooks(books)) {
+            throw new BookAlreadyOwnedException();
+        }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+        this.books = books;
+    }
 
-  public String getUsername() {
-    return username;
-  }
+    private boolean haveDuplicateBooks(List<Book> books) {
+        return countDuplicates(this.books, books) != 0 || countDuplicates(books, books) != 0;
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public LocalDate getBirthDate() {
-    return birthDate;
-  }
-
-  public void setBirthDate(LocalDate birthDate) {
-    this.birthDate = birthDate;
-  }
-
-  public List<Book> getBooks() {
-    return (List<Book>) Collections.unmodifiableList(books);
-  }
-
-  public void setBooks(List<Book> books) {
-    this.books = books;
-  }
+    private long countDuplicates(List<Book> searchIn, List<Book> search) {
+        return searchIn.stream().filter(book -> Collections.frequency(search, book) > 1).count();
+    }
 }
