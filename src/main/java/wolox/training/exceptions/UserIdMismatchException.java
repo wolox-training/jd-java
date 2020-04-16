7a0443189c58c0dfccbcfb5af504daeaf6d8677e
Jorge Diaz <jorge.diaz@wolox.co>
2020-04-16 15:21:31 -0500

    fix typo on exception classes

--- src/main/java/wolox/training/exceptions/UserIdMismatchException.java
+++ src/main/java/wolox/training/exceptions/UserIdMismatchException.java
@@ -3,7 +3,7 @@ package wolox.training.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Book Id mismatch")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "User Id mismatch")
 public class UserIdMismatchException extends Exception {
 
 }
