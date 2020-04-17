cd15e3dfce16a7837819c8df4b206e4bc712e0c4
Jorge Diaz <jorge.diaz@wolox.co>
2020-04-17 08:16:26 -0500

    modify swagger configuration

--- src/main/java/wolox/training/config/SwaggerConfig.java
+++ src/main/java/wolox/training/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package wolox.training.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                    .select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build()
@@ -29,4 +31,5 @@ public class SwaggerConfig {
                    .version("1.0.0")
                    .build();
     }
+
 }
