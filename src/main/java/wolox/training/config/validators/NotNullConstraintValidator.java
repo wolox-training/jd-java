86dd4c8a5c7fb944d722cfd2def7f877c46cf367
Jorge Diaz <jorge.diaz@wolox.co>
2020-04-20 09:50:44 -0500

    Not null validator apply to whole data types

--- src/main/java/wolox/training/config/validators/NotNullConstraintValidator.java
+++ src/main/java/wolox/training/config/validators/NotNullConstraintValidator.java
@@ -5,10 +5,10 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import wolox.training.config.annotations.NotNullConstraint;
 
-public class NotNullConstraintValidator implements ConstraintValidator<NotNullConstraint, String> {
+public class NotNullConstraintValidator implements ConstraintValidator<NotNullConstraint, Object> {
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
         Preconditions.checkNotNull(value, "Value can not be null");
         return true;
     }
