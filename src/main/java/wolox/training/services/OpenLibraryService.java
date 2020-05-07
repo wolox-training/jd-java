41d96f0e1ff2939e093b8aee15201f23ee748511
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-07 09:49:35 -0500

    Call service openlibrary, bookDTO from service to book model

--- src/main/java/wolox/training/services/OpenLibraryService.java
+++ src/main/java/wolox/training/services/OpenLibraryService.java
@@ -1,5 +1,53 @@
 package wolox.training.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import wolox.training.exceptions.BookNotFoundException;
+import wolox.training.helpers.Json;
+import wolox.training.services.dtos.BookDTO;
+
 public class OpenLibraryService {
+
+    private HttpClient httpClient;
+
+    private HttpRequest httpRequest;
+
+    private static final String OPEN_LIBRARY_URI = "https://openlibrary.org/api/books";
+
+    private static final String OPEN_LIBRARY_PARAMS = "?bibkeys=ISBN:%s&format=json&jscmd=data";
+
+    private String url;
+
+    public OpenLibraryService() {
+        this.httpClient = HttpClient.newHttpClient();
+        this.url = OPEN_LIBRARY_URI + OPEN_LIBRARY_PARAMS;
+    }
+
+    public wolox.training.pojos.dtos.BookDTO bookInformation(String isbn)
+        throws InterruptedException, IOException, URISyntaxException, BookNotFoundException {
+        HttpResponse<String> response = this.makeRequest(isbn);
+        JsonNode json = Json.stringToJson(response.body());
+
+        if (json.isEmpty()) {
+            throw new BookNotFoundException();
+        } else {
+            return new BookDTO(isbn, json).convertToDTO();
+        }
+    }
+
+    private HttpResponse<String> makeRequest(String isbn)
+        throws URISyntaxException, IOException, InterruptedException {
+        this.httpRequest = HttpRequest.newBuilder()
+                               .uri(new URI(String.format(this.url, isbn)))
+                               .build();
+
+        return this.httpClient.send(this.httpRequest, BodyHandlers.ofString());
+    }
 
 }
--- src/main/java/wolox/training/services/dtos/BookDTO.java
+++ src/main/java/wolox/training/services/dtos/BookDTO.java
@@ -1,5 +1,73 @@
 package wolox.training.services.dtos;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class BookDTO {
+
+    private JsonNode json;
+
+    private String isbn;
+
+    public BookDTO(String isbn, JsonNode json) {
+        this.isbn = isbn;
+        this.json = json.get("ISBN:" + isbn);
+    }
+
+    public wolox.training.pojos.dtos.BookDTO convertToDTO() {
+        return new wolox.training.pojos.dtos.BookDTO(
+            0,
+            this.getGenre(),
+            this.getAuthor(),
+            this.getImage(),
+            this.getTitle(),
+            this.getSubtitle(),
+            this.getPublisher(),
+            this.getYear(),
+            this.getPages(),
+            this.isbn
+        );
+    }
+
+    private String getGenre() {
+        String genre = this.getStringFromArray(this.json.get("subjects"));
+        return genre.length() > 200 ? genre.substring(0, 200) : genre;
+    }
+
+    private String getAuthor() {
+        return this.getStringFromArray(this.json.get("authors"));
+    }
+
+    private String getImage() {
+        return this.json.get("cover").get("large").asText();
+    }
+
+    private String getTitle() {
+        return this.json.get("title").asText();
+    }
+
+    private String getSubtitle() {
+        return this.json.has("subtitle") ? this.json.get("subtitle").asText() : "No specified";
+    }
+
+    private String getPublisher() {
+        return this.getStringFromArray(this.json.get("publishers"));
+    }
+
+    private String getYear() {
+        return this.json.get("publish_date").asText();
+    }
+
+    private int getPages() {
+        return this.json.get("number_of_pages").asInt();
+    }
+
+    private String getStringFromArray(JsonNode elements) {
+        String elementsJoined = "";
+        for (JsonNode element : elements) {
+            elementsJoined += elementsJoined.isEmpty() ? "" : ",";
+            elementsJoined += element.get("name").asText();
+        }
+        return elementsJoined;
+    }
 
 }
