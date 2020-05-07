c1a2d71edd9ca4d358acb36588bba85d69ff138f
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-07 09:49:00 -0500

    book DTO to create a book model

--- src/main/java/wolox/training/pojos/dtos/BookDTO.java
+++ src/main/java/wolox/training/pojos/dtos/BookDTO.java
@@ -1,5 +1,41 @@
 package wolox.training.pojos.dtos;
 
+import java.text.ParseException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+import wolox.training.models.Book;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class BookDTO {
+
+    private long id;
+
+    private String genre;
+
+    private String author;
+
+    private String image;
+
+    private String title;
+
+    private String subtitle;
+
+    private String publisher;
+
+    private String year;
+
+    private int pages;
+
+    private String isbn;
+
+    public Book convertToEntity() throws ParseException {
+        ModelMapper modelMapper = new ModelMapper();
+        Book book = modelMapper.map(this, Book.class);
+        return book;
+    }
 
 }
