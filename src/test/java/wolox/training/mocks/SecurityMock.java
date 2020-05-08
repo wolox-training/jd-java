411498de24bad031532d1977d7cb7c46b904cffa
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-08 12:26:36 -0500

    security mock authentication

---
+++ src/test/java/wolox/training/mocks/SecurityMock.java
@@ -0,0 +1,18 @@
+package wolox.training.mocks;
+
+import org.mockito.Mockito;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityMock {
+
+    public static void authentication() {
+        Authentication authentication = Mockito.mock(Authentication.class);
+        SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        Mockito.when(authentication.getPrincipal()).thenReturn("user");
+    }
+
+}
