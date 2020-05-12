f3bc3849fd083706f8a42580c5b7e7c8ba7ffe9d
Jorge Diaz <jorge.diaz@wolox.co>
2020-05-12 09:44:58 -0500

    pageable configuration

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -3,8 +3,10 @@ package wolox.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableSpringDataWebSupport
 public class TrainingApplication {
 
     public static void main(String[] args) {
